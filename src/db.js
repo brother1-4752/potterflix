192337f2508ecadcc1e9d54ae49bdd00b1bb2e2b
brother1-4752 <60454376+brother1-4752@users.noreply.github.com>
2023-03-12 13:32:03 +0900

    feat : webpack 9.1강까지

--- src/db.js
+++ src/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/potterflix", {
+console.log(process.env.DB_URL);
+
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
 });
 
 const db = mongoose.connection;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
+
+console.log(process.env.COOKIE_SECRET);
 
 const app = express();
 const logger = morgan("dev");
@@ -11,7 +16,20 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
-app.use("/", globalRouter);
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+    }),
+  })
+);
+
+app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
